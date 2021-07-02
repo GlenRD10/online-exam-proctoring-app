c99a0dfd95b1fdd174cd2f235317277dc2daf6c2
GlenRD10 <glen123dsouza@gmail.com>
2021-07-02 12:45:13 +0530

    Setting up the project

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Online Exam Proctoring</h1>
+        <p className="text-muted">Application for online exam proctoring</p>
+      </div>
+    </div>
+  );
+}
